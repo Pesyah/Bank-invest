6423d9f9098fc39727829acddbd38def5bd1c4c8
Artur <xxarturkrossxx@yandex.ru>
2021-10-06 15:34:40 +0300

    add From for test

---
+++ src/components/organizms/TestForm/TestForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TestForm.css'
+
+const TestForm = () => {
+    return (
+        <div>
+            <div class = 'test'>
+                <div id = 'main'>
+                    <p>текст</p>
+                </div>
+            </div>  
+        </div>
+    );
+};
+
+export default TestForm;
