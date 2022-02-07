06288fbdb48fe55f73be8579d8b9674f227b34f4
Artur <xxarturkrossxx@yandex.ru>
2022-02-07 11:12:01 +0300

    Починил кнопку
    Теперь кнопка выхода из аккаунта всегда будет "выйти".

--- src/App.js
+++ src/App.js
@@ -10,48 +10,44 @@ import ModalWindow from './components/molecules/ModalWndow/ModalWindow';
 function App() {
 
   const [isAuth, setAuth] = useState(JSON.parse(localStorage.getItem('Auth')) || false)
-  const [btnName, setBtnName] = useState('Войти')
   const [login, setLogin] =useState('')
   const [password, setPassword] =useState('')
   const [arrUsers, setArrUsers] =useState(users)
   const [ModalActive, setModalActive] =useState(false)
   const [money, setMoney] = useState( JSON.parse(localStorage.getItem('money')) ||0)
 
-
   function registration(){
     setAuth(true)
     setBtnName('Войти')
     localStorage.setItem('Auth', JSON.stringify('true'))
-   
   }
 
-  function submit(){
+  function submit(){ // Функция проверяет введён ли верно лог и пас.
     
     for(let i of arrUsers){
       if(i.login===login&&i.password===password){
         setAuth(false)
         setBtnName('Выйти')
-        console.log(1)
         localStorage.setItem('Auth', JSON.stringify(false))
       }
     }
   }
   return (
 
-    JSON.parse(localStorage.getItem('Auth')) ? 
+    JSON.parse(localStorage.getItem('Auth')) ?  // Надо входить?
       <RegForm 
       login={(e)=> setLogin(e.target.value)}
       password={(e)=> setPassword(e.target.value)}
       name='отправить' 
       onClick={submit}/> 
-    :
+    : // Если входить не нужно, пользователь авторизован.
     <>
     <div className="count">
       <p className="count-p"><div className="money"></div><span>{money}</span></p>
       <button onClick={()=> setModalActive(true)}>ok</button>
     </div>
       <DefaultPage 
-      name = {btnName}
+      name = {'Выйти'}
       onClick={registration}/>
       {/* <Shop money={money}/> */}
       <ModalWindow 
